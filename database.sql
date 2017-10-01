9cf295a8af3bc85ee62c4c358d8c253a91506bb1
Rizqy H <rizqy22@gmail.com>
2017-10-02 00:02:05 +0700

    Fix default database migration and add default admin account

--- database.sql
+++ database.sql
@@ -2,9 +2,9 @@ CREATE TABLE `admin` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `created_at` datetime NOT NULL
+  `created_at` datetime NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`);
+  UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `member` (
@@ -16,6 +16,8 @@ CREATE TABLE `member` (
   `nama` varchar(255) DEFAULT NULL,
   `noHp` varchar(255) DEFAULT NULL,
   `noReg` int(11) NOT NULL,
-  `status` tinyint(1) NOT NULL
-  PRIMARY KEY (`nim`);
+  `status` tinyint(1) NOT NULL,
+  PRIMARY KEY (`nim`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `admin` (`id`, `email`, `password`, `created_at`) VALUES (NULL, 'admin@amcc.or.id', '$2y$10$vahBv3GevBuExSoYwH5Ps.UszOT4fW4k1Z2hBEwi8.sIwk88Fv8wC', NOW());
